734ec74ae54845e1192acda72620c19feb0f2e92
Bander Saleh <bandersaleh93@gmail.com>
2023-02-23 10:20:34 -0800

    Update Team_SortBudgeDsc.cs

--- Team_SortBudgeDsc.cs
+++ Team_SortBudgeDsc.cs
@@ -8,6 +8,8 @@ namespace Prog_124_Lecture_12_ICompareable
 {
     internal class Team_SortBudgeDsc : IComparer<Team>
     {
+
+        // IComparer is used to compare x2 different external Instances rather than comparing to itself
         public int Compare(Team x, Team y)
         {
             //if (x.Budget > y.Budget) return 1;
